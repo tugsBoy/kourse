ef64e1320c638ec290c373ce0800e5f39e2f88e2
Tugs Boy <social_media_bs@protonmail.com>
2023-03-19 22:25:40 -0500

    syntax

--- src/api/webApi.go
+++ src/api/webApi.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func HandleREquests() {
+func HandleRequests() {
 	route1 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Welcome to your workflow!\n")
 	}
--- src/runApi.go
+++ src/runApi.go
@@ -3,5 +3,5 @@ package main
 import "kourse/src/api"
 
 func main() {
-	api.HandleREquests()
+	api.HandleRequests()
 }
